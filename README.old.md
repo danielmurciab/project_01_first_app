a972d6b385fcd0058fbbfbd21165b9cfecf37174
Daniel Murcia <danielmurcianetwork@gmail.com>
2024-06-27 08:07:13 -0500

    insert word HOLA

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# project_01_first_app
+First project in Talento Tech
---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+  return (
+    <div>
+        <div>MyFirstComponent</div>
+        <div>Hello World, I´m Daniel Murcia</div>
+        <div></div>
+    </div>
+  )
+}
